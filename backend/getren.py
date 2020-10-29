9107edcc7c1241fdd634f3fb6882074fce69dc6f
João Gabriel Lembo <joaogabriellembo@usp.br>
2020-10-29 19:24:17 -0300

    Cria novos modelos no db
    Adiciona as tabelas para curso e video, e as relações entre usuários, cursos e vídeos.

--- backend/getren.py
+++ backend/getren.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User, Curso
+from app.models import User, Course
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Curso': Curso}
+    return {'db': db, 'User': User, 'Curso': Course}
