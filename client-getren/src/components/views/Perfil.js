2671dc8580c909fea5371b2c5f42a63c50d77be9
availablenick <leoap711@gmail.com>
2020-11-14 01:37:52 -0300

    Atualize página de perfil
    Ajusta requisições da API localidades, ajusta campos do formulário e
    configura inicialmente a atualização de perfil.
    
    Signed-off-by: "Victor Pereira Lima <victor.p.lima@usp.br>"
    Signed-off-by: "Leonardo Alves Pereira <leoap711@gmail.com>"

--- client-getren/src/components/views/Perfil.js
+++ client-getren/src/components/views/Perfil.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import axios from 'axios';
 
-
 class Perfil extends React.Component {
   constructor(props) {
     super(props);
@@ -14,66 +13,141 @@ class Perfil extends React.Component {
 
     this.state = {
       user: {},
-      selected_state: null,
+      federal_states_options_list: [],
+      cities_options_list: [],
+      errors: {},
+      update_succeeded: false
     };
-    this.states_list = [];
-
-    axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+  }
+  
+  componentDidMount = () => {
+    let url = 'http://localhost:5000/user/' + this.props.user.data.id;
+    axios.get(url)
       .then(response => {
-          this.states_list = response.data.map((state, index) => {
+        if (response.data.status === 200) {
+          this.setState({
+            user: response.data.user,
+          });
+        }
+
+        return axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+      })
+      .then(response => {
+          let federal_states_options_list = response.data.map((state, index) => {
             return (
-              <option value={state.sigla} key={index} 
-                selected={this.state.user.estado === state.sigla}>
+              <option value={state.sigla} key={index}>
                 {state.sigla}
               </option>
             );
           });
-      });
-    let url = 'http://localhost:5000/user/' + props.user.data.id;
-    axios.get(url)
+          this.setState({
+            federal_states_options_list: federal_states_options_list
+          });
+          return this.requestCitiesList(this.state.user.estado);
+      })
       .then(response => {
-        if (response.data.status === 200) {
-          this.setState({user: response.data.user, selected_state: response.data.user.estado});
-        }
+        this.setState({ cities_options_list: response });
       });
   }
 
   render() {
-    return ( 
+    return (
       <div>
         <h2>Perfil</h2><br/>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <span>Nome</span>
-          <input type="text" value={this.state.user.nome} />
+          <input type="text" name="name"
+            defaultValue={this.state.user.nome !== null ? this.state.user.nome : ''}
+          />
           <br/>
           <span>Email</span>
-          <input type="text" value={this.state.user.email} disabled/>
+          <input type="text" name="email"
+            defaultValue={this.state.user.email !== null ? this.state.user.email : ''}
+            disabled
+          />
+          <br/>
           <span>Profissão</span>
-          <input type="text" value={this.state.user.profissao} />
+          <input type="text" name="job"
+            defaultValue={this.state.user.profissao !== null ? this.state.user.profissao : ''}
+          />
           <br/>
           <span>Data nascimento</span>
-          <input type="date" value={this.state.user.data_nascimento} />
+          <input type="date" name="birthdate"
+            defaultValue={this.state.user.data_nascimento !== null ? this.state.user.data_nascimento : ''}
+          />
           <br/>
           <span>Estado</span>
-          <select name="estado" onChange={this.handleChangeStates}>
-            {this.states_list}
+          <select name="federal_state" defaultValue={this.state.user.estado}
+            onChange={this.handleChangeStates}
+          >
+            {this.state.federal_states_options_list}
           </select>
           <br/>
           <span>Cidade</span>
-          <select name="cidade" value={this.state.user.cidade}>
+          <select name="city" defaultValue={this.state.user.cidade}>
+            {this.state.cities_options_list}
           </select>
           <br/>
           <button>Atualizar</button>
         </form>
+
+        {this.state.update_succeeded &&
+          <span>Perfil atualizado com sucesso</span>
+        }
       </div>
     );
   }
 
   handleChangeStates = (event) => {
-    this.setState({selected_state: event.target.value});
-    console.log(this.state.selected_state);
+    event.persist();
+    this.requestCitiesList(event.target.value)
+      .then(response => {
+        this.setState({
+          cities_options_list: response
+        });
+      });
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let form_data = new FormData(event.target);
+    let request_data = {};
+    for (let [key, value] of form_data.entries()) {
+      request_data[key] = value;
+    }
+    axios.put('http://localhost:5000/user/' + this.props.user.data.id, request_data)
+      .then(response => {
+        if (response.data.status === 200) {
+          this.setState({
+            user: response.data.user,
+            update_succeeded: true
+          });
+        }
+      });
+  }
+
+  requestCitiesList = (federal_state) => {
+    let cities_options_list = [];
+    let federal_state_param = this.state.federal_states_options_list[0].props.value;
+    if (federal_state !== null) {
+      federal_state_param = federal_state;
+    }
+    let url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/' + 
+      federal_state_param + '/municipios';
+    return new Promise(resolve => {
+      axios.get(url)
+        .then(response => {
+          cities_options_list = response.data.map((city, index) => {
+            return (
+              <option value={city.nome} key={index}>
+                  {city.nome}
+              </option>
+            );
+          });
+          resolve(cities_options_list);
+        });
+    });
+  }
 }
 
 const mapStateToProps = (state) => {
